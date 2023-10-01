2b474434d933123e63b53fc65651abe5978506dc
mayank sahu <132261449+Mayank8866@users.noreply.github.com>
2023-10-01 10:55:10 +0530

    Create H.java

---
+++ Hospital Management/H.java
@@ -0,0 +1,11 @@
+public class H {
+    public static void main(String[] args) {
+        System.out.println("HI.....");
+        Caller c=new Caller();
+        c.call();
+        Homepage hp=new Homepage();
+        hp.hPage(); 
+      
+      
+    }
+}
